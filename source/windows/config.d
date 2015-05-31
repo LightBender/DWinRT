7acddba2d9500d01415d1331459852b37de68b1e
Adam Wilson <adamw@ellipticbit.com>
2015-05-31 01:29:07 -0700

    Initial Commit. Setup basic project structure. Added IInspectable interface.

---
+++ source/windows/config.d
@@ -0,0 +1,23 @@
+﻿module windows.config;
+
+version(Windows80)
+{
+	enum bool Runtime80 = true;
+	enum bool Runtime81 = false;
+	enum bool Runtime100 = false;
+}
+else version(Windows81)
+{
+	enum bool Runtime80 = true;
+	enum bool Runtime81 = true;
+	enum bool Runtime100 = false;
+}
+else version(Windows100)
+{
+	enum bool Runtime80 = true;
+	enum bool Runtime81 = true;
+	enum bool Runtime100 = true;
+}
+else{
+	static assert(0);
+}
---
+++ source/windows/iinspectable.d
@@ -0,0 +1,20 @@
+﻿module windows.iinspectable;
+
+import windows.com;
+
+public enum TrustLevel {
+	BaseTrust		= 0,
+	PartialTrust	= 1,
+	FullTrust		= 2,
+}
+
+mixin(uuid!(IInspectable, "AF86E2E0-B12D-4c6a-9C5A-D7AA65101E90"));
+public interface IInspectable : IUnknown
+{
+	extern(Windows)
+	{
+		HRESULT GetIids(ulong *iidCount, IID **iids);
+		HRESULT GetRuntimeClassName(HSTRING *className);
+		HRESULT GetTrustLevel(TrustLevel *trustLevel);
+	}
+}
